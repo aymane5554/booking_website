9e411a4e9ee1a7abdf078fb25508ece97f5d38ea
aymane5554 <aymane.arrouji@gmail.com>
2024-04-20 11:28:02 +0100

    second commit

--- final_project/settings.py
+++ final_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'final_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bobakhqrrcmsvlcizzvo',
+        'USER': 'uqhqnvkg4s61ackpatp5',
+        'PASSWORD': 'UTBcVSaVdcnmeyNOrTqmREOEC3eLBS',
+        'HOST': 'bobakhqrrcmsvlcizzvo-postgresql.services.clever-cloud.com',
+        'PORT': '50013',
     }
 }
 
